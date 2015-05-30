bfb4eb044b48dd7d9b69d5a3c376070d8ed2db9d
Tim Buckingham <mr@timbuckingham.com>
2015-05-30 19:41:51 -0400

    Fixed settings not coming in the package (and therefor failing to save. Thanks 3K.Studios on the forums.

--- modules/btx-form-builder/_header.php
+++ modules/btx-form-builder/_header.php
@@ -1,5 +1,10 @@
 <?
 	$bigtree["css"][] = "btx-form-builder.css";
 	$bigtree["js"][] = "btx-form-builder.js";
+
+	// Create the settings array if it doesn't exist
+	if (!$admin->settingExists("settings")) {
+		$admin->createSetting(array("id" => "settings","system" => "on","name" => "Form Builder Settings"));
+	}
 	$settings = $cms->getSetting("settings");
 ?>
