781342be0369c1ffcb7fdcbccab2bd616f526e08
Tim Buckingham <mr@timbuckingham.com>
2024-10-10 20:20:08 -0400

    Fixing export when some fields have invalid data due to automated submissions

--- modules/btx-form-builder/export.php
+++ modules/btx-form-builder/export.php
@@ -2,21 +2,21 @@
 	/**
 	 * @global array $bigtree
 	 */
-	
+
 	$form = BTXFormBuilder::getForm($bigtree["commands"][0]);
-	
+
 	header("Content-type: text/csv");
 	header('Content-Disposition: attachment; filename="'.$cms->urlify($form["title"])."-".date("Y-m-d").'.csv"');
-	
+
 	$draw_csv_header = function ($fields) {
 		global $draw_csv_header;
-		
+
 		$columns = [];
-		
+
 		foreach ($fields as $field) {
 			$fdata = json_decode($field["data"], true);
 			$label = str_replace('"', '""', (!empty($fdata["label"]) ? $fdata["label"] : ucwords($field["type"])));
-			
+
 			if ($field["type"] == "column") {
 				$columns = array_merge($columns, $draw_csv_header($field["fields"]));
 			} elseif ($field["type"] == "address") {
@@ -33,21 +33,25 @@
 				$columns[] = $label;
 			}
 		}
-		
+
 		return $columns;
 	};
-	
+
 	$draw_csv_record = function ($entry, $fields) {
 		global $draw_csv_record;
-		
+
 		$record = [];
-		
+
 		foreach ($fields as $field) {
-			$value = $entry["data"][$field["id"]];
-			
+			$value = $entry["data"][$field["id"]] ?? "";
+
 			if ($field["type"] == "column") {
 				$record = array_merge($record, $draw_csv_record($entry, $field["fields"]));
 			} elseif ($field["type"] == "address") {
+				if (empty($value)) {
+					$value = ["street" => "", "street2" => "", "city" => "", "state" => "", "zip" => "", "country" => ""];
+				}
+
 				$record[] = str_replace('"', '""', $value["street"]);
 				$record[] = str_replace('"', '""', $value["street2"]);
 				$record[] = str_replace('"', '""', $value["city"]);
@@ -55,6 +59,10 @@
 				$record[] = str_replace('"', '""', $value["zip"]);
 				$record[] = str_replace('"', '""', $value["country"]);
 			} elseif ($field["type"] == "name") {
+				if (empty($value)) {
+					$value = ["first" => "", "last" => ""];
+				}
+
 				$record[] = str_replace('"', '""', $value["first"]);
 				$record[] = str_replace('"', '""', $value["last"]);
 			} elseif ($field["type"] == "checkbox") {
@@ -67,18 +75,18 @@
 				$record[] = str_replace('"', '""', $value);
 			}
 		}
-		
+
 		return $record;
 	};
-	
+
 	// Draw Header
 	echo '"'.implode('","', $draw_csv_header($form["fields"])).'","Date Submitted"'."\n";
-	
+
 	// Draw Data
 	$entries = BTXFormBuilder::getEntries($form["id"]);
-	
+
 	foreach ($entries as $entry) {
 		echo '"'.implode('","', $draw_csv_record($entry, $form["fields"])).'","'.$entry["created_at"].'"'."\n";
 	}
-	
+
 	die();
